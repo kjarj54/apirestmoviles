7016e2205512788f7294364070fdd423bd64d145
Arauz <kjarj54@gmail.com>
2025-03-25 10:05:36 -0600

    [REFactor] Update CORS configuration to allow additional origin for production

--- src/main/java/com/restapi/apirestmoviles/config/CorsConfiguration.java
+++ src/main/java/com/restapi/apirestmoviles/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT","*")
-                .allowedOrigins("http://127.0.0.1:5500","*")
+                .allowedOrigins("http://127.0.0.1:5500","*","http://apirestmoviles-production.up.railway.app")
                 .allowedHeaders("*");
     }
 }
