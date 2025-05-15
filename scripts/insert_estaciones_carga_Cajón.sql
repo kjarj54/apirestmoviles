116350c7557a21a9932b3fdf76c2225d7bbaae78
Anthony Avila <105742243+Anthonyah131@users.noreply.github.com>
2025-05-15 15:50:02 -0600

    Create insert_estaciones_carga_Cajón.sql

---
+++ scripts/insert_estaciones_carga_Cajón.sql
@@ -0,0 +1,80 @@
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #1', 'Ruta 8, Sector Sur, Pérez Zeledón', 'Tipo 2', 50, 0.33, '06:00-22:00', 9.366845045045045, -83.6171, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #2', 'Ruta 9, Sector Este, Pérez Zeledón', 'CHAdeMO', 150, 0.45, '06:00-22:00', 9.353651656810204, -83.58482208614865, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #3', 'Ruta 14, Sector Sur, Pérez Zeledón', 'CHAdeMO', 150, 0.13, '06:00-22:00', 9.3218, -83.5714521364663, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #4', 'Ruta 2, Sector Sur, Pérez Zeledón', 'Tipo 1', 22, 0.15, '06:00-22:00', 9.289948343189796, -83.58482208614865, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #5', 'Ruta 6, Sector Sur, Pérez Zeledón', 'Tipo 1', 100, 0.47, '06:00-22:00', 9.276754954954955, -83.6171, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #6', 'Ruta 9, Sector Norte, Pérez Zeledón', 'CHAdeMO', 100, 0.2, '06:00-22:00', 9.289948343189796, -83.64937791385134, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #7', 'Ruta 29, Sector Norte, Pérez Zeledón', 'Tipo 1', 150, 0.49, '06:00-22:00', 9.3218, -83.66274786353368, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #8', 'Ruta 10, Sector Oeste, Pérez Zeledón', 'CHAdeMO', 50, 0.34, '06:00-22:00', 9.353651656810204, -83.64937791385134, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #9', 'Ruta 28, Sector Este, Pérez Zeledón', 'Tipo 2', 50, 0.11, '06:00-22:00', 9.408820344710726, -83.59347092709848, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #10', 'Ruta 16, Sector Sur, Pérez Zeledón', 'CHAdeMO', 22, 0.35, '06:00-22:00', 9.38550331362041, -83.5525441722973, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #11', 'Ruta 11, Sector Oeste, Pérez Zeledón', 'Tipo 1', 50, 0.38, '06:00-22:00', 9.345117031090316, -83.52891509939577, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #12', 'Ruta 13, Sector Sur, Pérez Zeledón', 'Tipo 2', 50, 0.26, '06:00-22:00', 9.298482968909683, -83.52891509939577, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #13', 'Ruta 10, Sector Este, Pérez Zeledón', 'CCS', 22, 0.23, '06:00-22:00', 9.25809668637959, -83.5525441722973, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #14', 'Ruta 23, Sector Norte, Pérez Zeledón', 'Tipo 1', 50, 0.33, '06:00-22:00', 9.234779655289273, -83.59347092709848, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #15', 'Ruta 17, Sector Oeste, Pérez Zeledón', 'CHAdeMO', 100, 0.33, '06:00-22:00', 9.234779655289273, -83.64072907290151, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #16', 'Ruta 12, Sector Norte, Pérez Zeledón', 'Tipo 1', 150, 0.33, '06:00-22:00', 9.25809668637959, -83.68165582770268, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #17', 'Ruta 17, Sector Oeste, Pérez Zeledón', 'Tipo 1', 100, 0.35, '06:00-22:00', 9.298482968909683, -83.70528490060421, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #18', 'Ruta 3, Sector Sur, Pérez Zeledón', 'CHAdeMO', 100, 0.48, '06:00-22:00', 9.345117031090316, -83.70528490060421, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #19', 'Ruta 28, Sector Oeste, Pérez Zeledón', 'CCS', 50, 0.49, '06:00-22:00', 9.38550331362041, -83.68165582770268, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #20', 'Ruta 11, Sector Sur, Pérez Zeledón', 'Tipo 2', 22, 0.29, '06:00-22:00', 9.408820344710726, -83.64072907290151, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #21', 'Ruta 11, Sector Sur, Pérez Zeledón', 'Tipo 2', 150, 0.38, '06:00-22:00', 9.477840613294493, -83.52580427293262, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #22', 'Ruta 13, Sector Este, Pérez Zeledón', 'Tipo 2', 22, 0.23, '06:00-22:00', 9.442364073217812, -83.48140810568337, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #23', 'Ruta 25, Sector Oeste, Pérez Zeledón', 'CCS', 150, 0.31, '06:00-22:00', 9.395085881635278, -83.45029440647085, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #24', 'Ruta 13, Sector Sur, Pérez Zeledón', 'CCS', 50, 0.14, '06:00-22:00', 9.340633957345522, -83.43550880095586, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #25', 'Ruta 13, Sector Sur, Pérez Zeledón', 'Tipo 1', 150, 0.29, '06:00-22:00', 9.284338434086889, -83.4384986072236, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #26', 'Ruta 18, Sector Sur, Pérez Zeledón', 'CHAdeMO', 22, 0.33, '06:00-22:00', 9.23170990990991, -83.45897116220536, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #27', 'Ruta 7, Sector Este, Pérez Zeledón', 'CHAdeMO', 22, 0.18, '06:00-22:00', 9.187900031445514, -83.49492246957887, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #28', 'Ruta 23, Sector Norte, Pérez Zeledón', 'Tipo 1', 100, 0.41, '06:00-22:00', 9.15719721483917, -83.5428333648909, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #29', 'Ruta 26, Sector Norte, Pérez Zeledón', 'CHAdeMO', 100, 0.26, '06:00-22:00', 9.14260686569941, -83.59801399589348, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #30', 'Ruta 4, Sector Oeste, Pérez Zeledón', 'CHAdeMO', 50, 0.19, '06:00-22:00', 9.145557189056971, -83.65506289795802, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #31', 'Ruta 20, Sector Sur, Pérez Zeledón', 'CCS', 50, 0.48, '06:00-22:00', 9.165759386705506, -83.70839572706737, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #32', 'Ruta 4, Sector Oeste, Pérez Zeledón', 'CCS', 22, 0.31, '06:00-22:00', 9.201235926782187, -83.75279189431662, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #33', 'Ruta 8, Sector Oeste, Pérez Zeledón', 'CHAdeMO', 22, 0.26, '06:00-22:00', 9.24851411836472, -83.78390559352914, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #34', 'Ruta 3, Sector Sur, Pérez Zeledón', 'Tipo 2', 150, 0.33, '06:00-22:00', 9.302966042654477, -83.79869119904413, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #35', 'Ruta 7, Sector Norte, Pérez Zeledón', 'CCS', 150, 0.42, '06:00-22:00', 9.35926156591311, -83.7957013927764, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #36', 'Ruta 10, Sector Sur, Pérez Zeledón', 'Tipo 1', 50, 0.13, '06:00-22:00', 9.41189009009009, -83.77522883779463, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #37', 'Ruta 26, Sector Este, Pérez Zeledón', 'Tipo 2', 22, 0.15, '06:00-22:00', 9.455699968554486, -83.73927753042112, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #38', 'Ruta 7, Sector Oeste, Pérez Zeledón', 'Tipo 1', 150, 0.49, '06:00-22:00', 9.486402785160829, -83.69136663510909, FALSE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #39', 'Ruta 7, Sector Oeste, Pérez Zeledón', 'Tipo 2', 50, 0.27, '06:00-22:00', 9.50099313430059, -83.6361860041065, TRUE);
+INSERT INTO estaciones_carga ( nombre, direccion, tipo_cargador, potencia, tarifa, horario, latitud, longitud, disponibilidad)
+VALUES ('Estación VE #40', 'Ruta 14, Sector Sur, Pérez Zeledón', 'Tipo 1', 50, 0.37, '06:00-22:00', 9.498042810943028, -83.57913710204197, TRUE);
